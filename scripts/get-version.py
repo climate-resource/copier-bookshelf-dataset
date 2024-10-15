8baf08f9d078e1636edfd80969301799053704f1
Jared Lewis <jared@jared.kiwi.nz>
2024-10-15 22:34:34 +1100

    chore: Add towncrier config

---
+++ scripts/get-version.py
@@ -0,0 +1,29 @@
+"""
+Extract the current version from pyproject.toml and print to stdout
+
+These can then be used in our release template.
+"""
+
+from pathlib import Path
+
+import toml
+
+
+def get_version() -> str:
+    """
+    Extract the current version from pyproject.toml
+    """
+    version = "unknown"
+    # adopt path to your pyproject.toml
+    pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
+
+    if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
+        data = toml.load(pyproject_toml_file)
+        # check project.version
+        if "project" in data and "version" in data["project"]:
+            version = data["project"]["version"]
+    return version
+
+
+if __name__ == "__main__":
+    print(get_version())
